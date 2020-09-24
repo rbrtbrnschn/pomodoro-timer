08f2aac27d7a1ccfacfe5ac40f5a520209019726
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-09-24 13:19:30 +0200

    working

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cp -a $PWD "/usr/local/bin/pomodoro-timer"
 cp -a $PWD/pomodoro "/usr/local/bin/pomodoro"
 cp -a $PWD/pomodoro_timer "/usr/local/bin/pomodoro_timer"
 
-echo -e $POMODORO >> $HOME/.bashrc
-source $HOME/.bashrc
+#echo -e $POMODORO >> $HOME/.bashrc
+#source $HOME/.bashrc
 echo "Installation Complete"
 echo "You may have to 'source \$HOME/.bashrc'"
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf "/usr/local/bin/pomodoro-timer"
 rm -rf "/usr/local/bin/pomodoro_timer"
 rm -rf "/usr/local/bin/pomodoro"
 
